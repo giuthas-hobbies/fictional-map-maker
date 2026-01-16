9e0fd9f1d79ec6bc7003f812b37351812e82a420
Pertti Palo <giuthas@gmail.com>
2026-01-15 17:07:51 -0700

    Relatively fast voronoi plotting with random values for the cell colors.

--- src/fimama/__init__.py
+++ src/fimama/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
@@ -8,10 +10,7 @@ matplotlib.use('qtagg')
 def main() -> None:
     # make up data points
     np.random.seed(1234)
-    grid_shape = (100,100)
-    # Source - https://stackoverflow.com/a
-    # Posted by Arrows
-    # Retrieved 2026-01-14, License - CC BY-SA 4.0
+    grid_shape = (200,200)
 
     # make up data points
     base_points = np.mgrid[
@@ -21,25 +20,29 @@ def main() -> None:
     base_points = base_points.reshape(2,-1).T
     points = base_points + np.random.random_sample(base_points.shape) - .5
 
-    # points = np.random.rand(25,2)
-
+    # TODO 0.2: better dummy points or better yet exclude outermost ring of
+    # points while keeping them saved as the basis of extending the map.
     # add 4 distant dummy points
     points = np.append(points, [[999,999], [-999,999], [999,-999], [-999,-999]], axis = 0)
 
     # compute Voronoi tessellation
-    base_voronoi = Voronoi(base_points)
     voronoi = Voronoi(points)
 
     # plot
-    fig = voronoi_plot_2d(vor=voronoi, show_vertices=False, show_points=False)
-    axes = fig.axes
-    # voronoi_plot_2d(vor=base_voronoi, ax=axes[0], show_vertices=False)
+    fig, ax = plt.subplots()
+    # voronoi_plot_2d(vor=voronoi, ax=ax, show_vertices=False, show_points=False)
 
     # colorize
+    polygons = []
     for region in voronoi.regions:
-        if not -1 in region:
-            polygon = [voronoi.vertices[i] for i in region]
-            plt.fill(*zip(*polygon), "b")
+        if not -1 in region and len(region) > 0:
+            polygon = Polygon([voronoi.vertices[i] for i in region], closed=True)
+            polygons.append(polygon)
+    poly_collection = PatchCollection(polygons)
+    colors = 100 * np.random.rand(len(polygons))
+    poly_collection.set_array(colors)
+    ax.add_collection(poly_collection)
+    fig.colorbar(poly_collection, ax=ax)
 
     # fix the range of axes
     plt.xlim([-1,grid_shape[0]+1])
