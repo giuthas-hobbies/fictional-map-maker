4d750ab4d239381c4e9f5f18df9e854d6325b3ed
Pertti Palo <giuthas@gmail.com>
2026-01-22 12:58:46 -0700

    Better image size for testing.

--- src/fimama/__init__.py
+++ src/fimama/__init__.py
@@ -9,23 +9,24 @@ matplotlib.use('qtagg')
 
 def main() -> None:
     # plot
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+    fig, (ax1) = plt.subplots(nrows=1, ncols=1, layout="constrained")
     ax1.set_aspect('equal', 'box')
-    ax2.set_aspect('equal', 'box')
+    # ax2.set_aspect('equal', 'box')
 
     width = 200
-    height = 200
-    terrain = perlin_map(width=width, height=height)
+    height = 125
+    terrain = perlin_map(width=width, height=height).T
     # terrain = np.arange(width*height).reshape((width,height))
 
     # Read the colormap
     tmp = []
-    for row in np.loadtxt( "geo-smooth.gpf" ):
+    for row in np.loadtxt( "dark-atlas.gpf" ):
         tmp.append( [ row[0], row[1:4] ] )
-    colormap = matplotlib.colors.LinearSegmentedColormap.from_list( "geo-smooth", tmp )
+    colormap = matplotlib.colors.LinearSegmentedColormap.from_list( "dark-atlas", tmp )
 
     voronoi_map(fig=fig, axes=ax1, heightmap=terrain, colormap=colormap)
 
     # Colored elevation map
-    ax2.imshow(X=terrain, cmap=colormap)
+    # ax2.imshow(X=terrain, cmap=colormap)
+    # plt.tight_layout()
     plt.show()
