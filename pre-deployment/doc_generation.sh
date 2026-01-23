d3df77897418ef231941b43e96e53700b2677448
Pertti Palo <giuthas@gmail.com>
2026-01-23 00:28:23 -0700

    Updated pdoc command.

--- pre-deployment/doc_generation.sh
+++ pre-deployment/doc_generation.sh
@@ -2,7 +2,7 @@
 uv run python -m licenseheaders -t devel/gpl-affero-v3.tmpl -y 2026 -o "Pertti Palo" -n "Fictional Map Maker" -u https://github.com/giuthas-hobbies/fictional-map-maker/  --exclude docs/* .pixi/* .idea/* .vscode/* .VSCodeCounter/* *.html *.md *.markdown *.js *.xml *.yaml -v -v -v
 
 # Generate docs with pdoc ...
-uv run devel/generate_docs.py
+uv run pre-deployment/generate_api_docs.py
 
 # and some diagrams to be linked as well.
 # Needs Graphviz to work properly
