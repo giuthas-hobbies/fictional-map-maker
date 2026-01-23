1b359d7edc6705b4e587f4667d72c65d98dad652
Pertti Palo <giuthas@gmail.com>
2026-01-23 00:33:38 -0700

    Update licenseheader command.

--- pre-deployment/doc_generation.sh
+++ pre-deployment/doc_generation.sh
@@ -1,5 +1,5 @@
 # Update licenseheaders
-uv run python -m licenseheaders -t devel/gpl-affero-v3.tmpl -y 2026 -o "Pertti Palo" -n "Fictional Map Maker" -u https://github.com/giuthas-hobbies/fictional-map-maker/  --exclude docs/* .pixi/* .idea/* .vscode/* .VSCodeCounter/* *.html *.md *.markdown *.js *.xml *.yaml -v -v -v
+uv run python -m licenseheaders -t pre-deployment/gpl-affero-v3.tmpl -y 2026 -o "Pertti Palo" -n "Fictional Map Maker" -u https://github.com/giuthas-hobbies/fictional-map-maker/  --exclude docs/* .pixi/* .idea/* .vscode/* .VSCodeCounter/* *.html *.md *.markdown *.js *.xml *.yaml -v -v -v
 
 # Generate docs with pdoc ...
 uv run pre-deployment/generate_api_docs.py
